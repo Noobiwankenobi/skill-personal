e198f3cf71438d0dfef8fdf5578cd64ad5419d9c
Julian Fidenzio Vido <julian.vido@anu.edu.au>
2021-05-04 10:58:44 +0100

    Purple monkey diswasher debug with Kathy Reid on 4 May 2021

--- __init__.py
+++ __init__.py
@@ -28,20 +28,20 @@ class PersonalSkill(MycroftSkill):
     def handle_who_made_you_intent(self, message):
         self.speak_dialog("who.made.me")
 
-    @intent_handler("WhoAreYou.intent")
-    def handle_who_are_you_intent(self, message):
-        name = self.config_core.get("listener", {}).get("wake_word",
-                                                        "mycroft")
-        name = name.lower().replace("hey ", "")
-        self.speak_dialog("who.am.i", {"name": name})
+  #  @intent_handler("WhoAreYou.intent")
+  #  def handle_who_are_you_intent(self, message):
+  #      name = self.config_core.get("listener", {}).get("wake_word",
+  #                                                      "mycroft")
+  #      name = name.lower().replace("hey ", "")
+  #      self.speak_dialog("who.am.i", {"name": name})
+
+    @intent_handler("PurpleMonkeyDishwasher.intent")
+    def handle_purple_monkey_dishwasher(self, message):
+        self.speak_dialog("purple.monkey.dishwasher")
 
     @intent_handler("WhatAreYou.intent")
     def handle_what_are_you_intent(self, message):
         self.speak_dialog("what.am.i")
-
-    @intent_handler("DoYouRhyme.intent")
-    def handle_do_you_rhyme(self, message):
-        self.speak_dialog("tell.a.rhyme")
 
     @intent_handler("DoYouDream.intent")
     def handle_do_you_dream(self, message):
